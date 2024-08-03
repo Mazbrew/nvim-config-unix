ae8f2affd59e68c03997b18a7ed72440b4d6e068
Mazbrew <peteryeoh50@gmail.com>
2024-08-03 14:43:26 +0800

    might give up on this and just use vscode with vim movements

--- lua/plugins/mason.lua
+++ lua/plugins/mason.lua
@@ -1,12 +1,23 @@
 return {
 	"williamboman/mason.nvim",
-    "williamboman/mason-lspconfig.nvim",
-    "neovim/nvim-lspconfig",
-
+	dependencies = {
+		"williamboman/mason-lspconfig.nvim",
+		"neovim/nvim-lspconfig",
+		"WhoIsSethDaniel/mason-tool-installer.nvim",
+	},
 	config = function()
 		require("mason").setup()
-		require("mason-lspconfig").setup{
+
+		-- language server
+		-- need to install cmake
+		require("mason-lspconfig").setup({
 			ensure_installed = { "lua_ls", "clangd", "cmake" },
-		}
-	end
+		})
+
+		-- formatters and linters
+		-- need to install clang-format
+		require("mason-tool-installer").setup({
+			ensure_installed = { "stylua" },
+		})
+	end,
 }
